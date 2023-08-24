3d4d6d98bf2b4384b298f5d827ba989bb58c223b
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-08-24 21:58:04 +0300

    adding add and associated changes

--- monty.h
+++ monty.h
@@ -67,5 +67,6 @@ void _push(stack_t **stack, unsigned int line_number);
 void _pint(stack_t **stack, unsigned int line_number);
 void _pop(stack_t **stack, unsigned int line_number);
 void _swap(stack_t **stack, unsigned int line_number);
+void _add(stack_t **stack, unsigned int line_number);
 
 #endif /* MONTY_H*/
