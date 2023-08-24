1db73d618e9797bcaacfc616fd54ce738fecc13c
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-08-24 22:23:38 +0300

    adding nop and associated changes

--- monty_functions.c
+++ monty_functions.c
@@ -61,6 +61,7 @@ instruct_func get_op_func(char *str)
 		{"pop", _pop},
 		{"swap", _swap},
 		{"add", _add},
+		{"nop", _nop},
 		{NULL, NULL},
 	};
 
---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * _nop - nop top of stack y second top stack
+ * @stack: pointer to lists for monty stack
+ * @line_number: number of line opcode occurs on
+ */
+
+void _nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
